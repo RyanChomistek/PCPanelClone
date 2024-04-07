15364abd526446faef253a53c70ad32d80a69b2b
Ryan Chomistek <ryan.chomistek@gmail.com>
2024-04-07 09:53:13 -0700

    adding debounce to switch

--- firmware/RotaryEncOverMCP.h
+++ firmware/RotaryEncOverMCP.h
@@ -26,86 +26,95 @@ typedef void (*SwitchPressedFunc)(int id);
  * */
 class RotaryEncOverMCP {
 public:
-    RotaryEncOverMCP(
-      Adafruit_MCP23017* mcp, byte pinDT, byte pinCLK, byte pinSW, 
-      RotaryTurnFunc turnFunc, SwitchPressedFunc switchPressedFunc, int id = 0)
-    : rot(pinDT, pinCLK), 
-      mcp(mcp),
-      pinDT(pinDT), pinCLK(pinCLK), pinSW(pinSW),
-      turnFunc(turnFunc), m_switchPressedFunc(switchPressedFunc), id(id) {
+  RotaryEncOverMCP(
+    Adafruit_MCP23017* mcp, byte pinDT, byte pinCLK, byte pinSW, 
+    RotaryTurnFunc turnFunc, SwitchPressedFunc switchPressedFunc, int id = 0)
+      : rot(pinDT, pinCLK), 
+    mcp(mcp),
+    pinDT(pinDT), pinCLK(pinCLK), pinSW(pinSW),
+    turnFunc(turnFunc), m_switchPressedFunc(switchPressedFunc), id(id) {
+  }
+
+  /* Initialize object in the MCP */
+  void init() {
+    if(mcp != nullptr) {
+      mcp->pinMode(pinDT, INPUT);
+      mcp->pullUp(pinDT, 0); //disable pullup on this pin
+      mcp->setupInterruptPin(pinDT, CHANGE);
+
+      mcp->pinMode(pinCLK, INPUT);
+      mcp->pullUp(pinCLK, 0); //disable pullup on this pin
+      mcp->setupInterruptPin(pinCLK, CHANGE);
+      
+      mcp->pinMode(pinSW, INPUT);
+      mcp->pullUp(pinSW, 1); //enable pullup on this pin
+      mcp->setupInterruptPin(pinSW, CHANGE);
     }
+  }
 
-    /* Initialize object in the MCP */
-    void init() {
-        if(mcp != nullptr) {
-            mcp->pinMode(pinDT, INPUT);
-            mcp->pullUp(pinDT, 0); //disable pullup on this pin
-            mcp->setupInterruptPin(pinDT,CHANGE);
+  /* On an interrupt, can be called with the value of the GPIOAB register (or INTCAP) */
+  void feedInput(uint16_t gpioAB) {
+    uint8_t pinValDT = bitRead(gpioAB, pinDT);
+    uint8_t pinValCLK = bitRead(gpioAB, pinCLK);
+    uint8_t event = rot.process(pinValDT, pinValCLK);
 
-            mcp->pinMode(pinCLK, INPUT);
-            mcp->pullUp(pinCLK, 0); //disable pullup on this pin
-            mcp->setupInterruptPin(pinCLK,CHANGE);
-            
-            mcp->pinMode(pinSW, INPUT);
-            mcp->pullUp(pinSW, 1); //enable pullup on this pin
-            mcp->setupInterruptPin(pinSW,CHANGE);
-        }
-    }
+    if(event == DIR_CW || event == DIR_CCW) {
+      //clock wise or counter-clock wise
+      bool clockwise = event == DIR_CW;
+      Direction direction = clockwise ? Direction::CW : Direction::CCW;
 
-    /* On an interrupt, can be called with the value of the GPIOAB register (or INTCAP) */
-    void feedInput(uint16_t gpioAB) {
-      uint8_t pinValDT = bitRead(gpioAB, pinDT);
-      uint8_t pinValCLK = bitRead(gpioAB, pinCLK);
-      uint8_t event = rot.process(pinValDT, pinValCLK);
+      m_cnt += clockwise ? 1 : -1;
 
-      if(event == DIR_CW || event == DIR_CCW) {
-        //clock wise or counter-clock wise
-        bool clockwise = event == DIR_CW;
-        Direction direction = clockwise ? Direction::CW : Direction::CCW;
-
-        m_cnt += clockwise ? 1 : -1;
-
-        //Call into action function if registered
-        if(turnFunc) {
-          turnFunc(direction, id, m_cnt);
-          return;
-        }
-      }
-
-      uint8_t pinValSW = bitRead(gpioAB, pinSW);
-      if(pinValSW == 0)
-      {
-        m_switchPressedFunc(id);
+      //Call into action function if registered
+      if(turnFunc) {
+        turnFunc(direction, id, m_cnt);
+        return;
       }
     }
 
-    /* Poll the encoder. Will cause an I2C transfer. */
-    void poll() {
-        if(mcp != nullptr) {
-            feedInput(mcp->readGPIOAB());
-        }
-    }
+    uint8_t pinValSW = bitRead(gpioAB, pinSW);
 
-    Adafruit_MCP23017* getMCP() {
-        return mcp;
+    if ((millis() - lastDebounceTime) > debounceDelay
+      && pinValSW == 0) 
+    {
+      //char serialBuffer[255];
+      //sprintf(serialBuffer, "switch pressed %lu|", millis() - lastDebounceTime);
+      //Serial.write(serialBuffer);
+      m_switchPressedFunc(id);
+      lastDebounceTime = millis();
     }
+  }
 
-    int getID() {
-        return id;
-    }
+  /* Poll the encoder. Will cause an I2C transfer. */
+  void poll() {
+      if(mcp != nullptr) {
+          feedInput(mcp->readGPIOAB());
+      }
+  }
+
+  Adafruit_MCP23017* getMCP() {
+      return mcp;
+  }
+
+  int getID() {
+      return id;
+  }
 
 private:
-    Rotary rot;                         /* the rotary object which will be created*/
-    Adafruit_MCP23017* mcp = nullptr;   /* pointer the I2C GPIO expander it's connected to */
-    uint8_t pinDT = 0;
-    uint8_t pinCLK = 0;           /* the pin numbers for output A and output B */
-    uint8_t pinSW = 0;           /* the pin numbers for output A and output B */
-    RotaryTurnFunc turnFunc = nullptr;  /* function pointer, will be called when there is an action happening */
-    SwitchPressedFunc m_switchPressedFunc = nullptr;  
-    int id = 0;                             /* optional ID for identification */
-    int m_cnt = 0;
+  Rotary rot;                         /* the rotary object which will be created*/
+  Adafruit_MCP23017* mcp = nullptr;   /* pointer the I2C GPIO expander it's connected to */
+  uint8_t pinDT = 0;
+  uint8_t pinCLK = 0;           /* the pin numbers for output A and output B */
+  uint8_t pinSW = 0;           /* the pin numbers for output A and output B */
+  RotaryTurnFunc turnFunc = nullptr;  /* function pointer, will be called when there is an action happening */
+  SwitchPressedFunc m_switchPressedFunc = nullptr;  
+  int id = 0;                             /* optional ID for identification */
+  int m_cnt = 0;
 
-    bool m_fSwitchState = false;
+  bool m_fSwitchState = false;
+
+  unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+  unsigned long debounceDelay = 150;    // the debounce time; increase if the output flickers
 };
 
 
